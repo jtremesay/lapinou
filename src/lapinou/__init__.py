47b19154c3e30831d5cd91f1e296663c8fc9a439
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-08-06 18:54:37 +0200

    can infer

--- src/lapinou/__init__.py
+++ src/lapinou/__init__.py
@@ -1,2 +1,13 @@
+import profile
+
+from dotenv import load_dotenv
+from pydantic_ai import Agent
+
+load_dotenv()  # Load environment variables from .env file
+
+agent = Agent("ollama:gemma4:cloud")
+
+
 def main() -> None:
-    print("Hello from lapinou!")
+    o = agent.run_sync("What is the capital of France?")
+    print(o.output)
