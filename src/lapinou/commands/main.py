2709900ca64cd2bb61b00aca01a5719424018264
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-08-06 20:00:15 +0200

    add gpl

--- src/lapinou/commands/main.py
+++ src/lapinou/commands/main.py
@@ -1,4 +1,21 @@
+# Lapinou - An agentic game engine
+# Copyright (C) 2026 Jonathan Tremesaygues
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from argparse import ArgumentParser, Namespace
+
+import pygame
 
 from lapinou.core.command import BaseCommand
 
@@ -10,4 +27,25 @@ class Command(BaseCommand):
         pass
 
     def handle(self, args: Namespace) -> None:
-        pass
+        pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            # poll for events
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # fill the screen with a color to wipe away anything from last frame
+            screen.fill("purple")
+
+            # RENDER YOUR GAME HERE
+
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+
+            clock.tick(60)  # limits FPS to 60
+
+        pygame.quit()
