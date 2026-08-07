4bd468788e08bfeef07736216268db563c4c482d
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-08-07 03:00:21 +0200

    remove dependency to game

--- src/lapinou/commands/run.py
+++ src/lapinou/commands/run.py
@@ -14,24 +14,46 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from argparse import ArgumentParser, Namespace
+from importlib import import_module
+from logging import getLogger
+from os import getenv
 
 import pygame
 
-from llmrpg.entrypoint import get_entrypoint_scene
-
 from ..core.command import BaseCommand
 from ..director import Director
+
+logger = getLogger(__name__)
 
 
 class Command(BaseCommand):
     help_text = "Run a game"
 
     def add_arguments(self, parser: ArgumentParser) -> None:
-        pass
+        parser.add_argument(
+            "-g",
+            "--game",
+            default=getenv("LAPINOU_GAME", "llmrpg"),
+            type=str,
+            help="The game to run (e.g., 'my_game')",
+        )
 
     def handle(self, args: Namespace) -> None:
         pygame.init()
         screen = pygame.display.set_mode((1280, 720))
+        try:
+            entry_point = import_module(f"{args.game}.entrypoint")
+        except ModuleNotFoundError:
+            logger.error("Error: The game '%s' could not be found.", args.game)
+            return
+        try:
+            get_entrypoint_scene = entry_point.get_entrypoint_scene
+        except AttributeError:
+            logger.error(
+                "Error: The game '%s' does not have a 'get_entrypoint_scene' function.",
+                args.game,
+            )
+            return
 
         director = Director(get_entrypoint_scene())
         director.run(screen)
